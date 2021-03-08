c1256c798d89e41c9a3095b440e14321cb42b0d2
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2021-03-08 16:46:18 +0100

    Small changes and updated README

---
+++ README.md
@@ -0,0 +1,9 @@
+Spelet går ut på att man ska äta upp de mindre bollarna för att bli större.
+Vid en viss storlek vinner man (kommer senare)
+
+Man kan ändra färg genom att trycka på den första bokstaven för varje färg. T.ex. för att byta till blå så trycker du på tangenten b.
+
+Att göra:
+- Man ska kunna äta de mindre bollarna för att få den stora att växa
+- Bestämma storlek för att vinna spelet
+- Ha en counter som visar nuvarande storlek
--- main.lua
+++ main.lua
@@ -1,65 +1,73 @@
 love.load = function ()
     print('The game has been loaded')
-end
 
-x = 0
-y = 150
-r = 30
-current_color = {1, 1, 1}
-randomPositionx1 = math.random() * 500
-randomPositiony1 = math.random() * 500
-
-
-seconds = 0
-
-love.draw = function ()
-    if seconds < 15 then 
-        love.graphics.print('This is a moving and growing circle', 50, 50)
-    end
-    -- local clock_display = 'seconds ' .. math.floor(seconds)
-    -- love.graphics.print(clock_display, 0, 0, 0, 2, 2)
-    if seconds > 5 and seconds < 15 then
-         love.graphics.print("You can also change it's color", 50, 75)
-        end
-    if seconds > 17 and seconds < 30 then
-        love.graphics.print('Eat the small circles to grow bigger', 50, 50)
-    end
-    love.graphics.setColor(current_color)
-    love.graphics.circle('fill', x, y, r)
-
--- Det lilla objektet som ska ätas
-    love.graphics.circle('line', randomPositionx1, randomPositiony1, 10)
-       
-end
-
-
-
-
-love.keypressed = function (pressed_key)
-    if pressed_key == 'b' then 
-        current_color = {0, 0, 1}
-    elseif pressed_key == 'g' then
-        current_color = {0, 1, 0}
-    elseif pressed_key == 'r' then
-        current_color = {1, 0, 0}
-    elseif pressed_key == 'y' then
-        current_color = {1, 1, 0}
+    x = 0
+    y = 150
+    r = 30
+    current_color = {1, 1, 1}
+    randomPositionx1 = love.math.random() * 500
+    randomPositiony1 = love.math.random() * 500
     
+    
+    seconds = 0
+    
+    love.draw = function ()
+        if seconds < 15 then 
+            love.graphics.print('This is a moving circle', 50, 50)
+        end
+        -- local clock_display = 'seconds ' .. math.floor(seconds)
+        -- love.graphics.print(clock_display, 0, 0, 0, 2, 2)
+        if seconds > 5 and seconds < 15 then
+             love.graphics.print("You can also change it's color by pressing the corresponding buttons", 50, 75)
+            end
+        if seconds > 17 and seconds < 30 then
+            love.graphics.print('Eat the small circles to grow bigger', 50, 50)
+        end
+        love.graphics.setColor(current_color)
+        love.graphics.circle('fill', x, y, r)
+    
+    -- Det lilla objektet som ska ätas
+        love.graphics.circle('line', randomPositionx1, randomPositiony1, 10)
+           
     end
+    
+    
+    
+    
+    love.keypressed = function (pressed_key)
+        if pressed_key == 'b' then 
+            current_color = {0, 0, 1}
+        elseif pressed_key == 'g' then
+            current_color = {0, 1, 0}
+        elseif pressed_key == 'r' then
+            current_color = {1, 0, 0}
+        elseif pressed_key == 'y' then
+            current_color = {1, 1, 0}
+        
+        end
+    end
+    
+    love.update = function (dt)
+     if x == randomPositionx1 and y == randomPositiony1 then
+             r = r + 100
+     end
+        seconds = seconds + dt
+        if love.keyboard.isDown('right') then
+            x = x + 1
+        elseif love.keyboard.isDown('left') then
+            x = x - 1
+        elseif love.keyboard.isDown('up') then
+            y = y - 1
+        elseif love.keyboard.isDown('down') then
+            y = y + 1
+        end
+        --  funkar ej
+        if seconds == 10 then
+            love.load()
+    end 
+    
+    
+    
 end
 
-love.update = function (dt)
- if x == randomPosition1 and y == randomPosition2 then
-         r = r + 100
- end
-    seconds = seconds + dt
-    if love.keyboard.isDown('right') then
-        x = x + 1
-    elseif love.keyboard.isDown('left') then
-        x = x - 1
-    elseif love.keyboard.isDown('up') then
-        y = y - 1
-    elseif love.keyboard.isDown('down') then
-        y = y + 1
-    end
 end
