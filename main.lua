ae973a916902f54625234a75cada110675382996
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2021-04-12 15:30:52 +0200

    Adjusted code structure

--- main.lua
+++ main.lua
@@ -11,9 +11,8 @@ love.load = function ()
     foodPosition = {
      foodX = love.math.random() * width,
      foodY = love.math.random() * height
-    } 
-    
-    
+    }   
+
     seconds = 0
     
     love.draw = function ()
@@ -22,10 +21,10 @@ love.load = function ()
         end
         if seconds > 5 and seconds < 10 then 
             love.graphics.print("Use the arrow keys to move", 50, 75)
-            end
+        end
         if seconds > 10 and seconds < 16 then
              love.graphics.print("You can change it's color by pressing the first letter of each color", 50, 50)
-            end
+        end
         if seconds > 16 and seconds < 21 then
             love.graphics.print('Eat the small circles to grow bigger', 50, 50)
         end
@@ -39,17 +38,10 @@ love.load = function ()
         
         love.graphics.setColor(current_color)
         love.graphics.circle('fill', x, y, r)
-
-     
-
-    
-    -- Det lilla objektet som ska ätas
-        love.graphics.circle('line', foodPosition.foodX, foodPosition.foodY, 10)
-           
+ 
+     -- Det lilla objektet som ska ätas
+        love.graphics.circle('line', foodPosition.foodX, foodPosition.foodY, 10)   
     end
-    
-    
-    
     
     love.keypressed = function (pressed_key)
         if pressed_key == 'b' then 
@@ -64,28 +56,22 @@ love.load = function ()
             current_color = {1, 0, 1}
         elseif pressed_key == 'o' then
             current_color = {1, 0.64705882352941, 0}
-
-        
         end
     end
-    
-
-  
 
     love.update = function (dt)
     
      dx = x - foodPosition.foodX;
      dy = y - foodPosition.foodY;
      distance = math.sqrt(dx * dx + dy * dy);
-
-if (distance < r + 10) then 
-    r = r + 10
+     if (distance < r + 10) then 
+     r = r + 10
 
         foodPosition = {
             foodX = love.math.random() * width,
             foodY = love.math.random() * height
            }
-end
+     end
     
         seconds = seconds + dt
         if love.keyboard.isDown('right') then
@@ -98,11 +84,9 @@ end
             y = y + 1
         end
         
-      
-      if r == 200 then
-        love.load()
-      end
+        if r == 200 then
+          love.load()
+        end
     end
-
- end
+end
 
